652c372f16234cfbaba3ad8daf3153e6ffbc3f61
Francisco <hidalgo_fco520@outlook.com>
2018-03-30 17:31:22 -0600

    Funcionalidad de crear contactos agregada, ¡Ya se pueden crear contactos!

--- contactos/contactos/NuevoContactoPage.xaml.cs
+++ contactos/contactos/NuevoContactoPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using contactos.Clases;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -16,5 +17,24 @@ namespace contactos
 		{
 			InitializeComponent ();
 		}
-	}
+
+        private void Button_Clicked(object sender, EventArgs args)
+        {
+            Contacto nuevoContacto = new Contacto()
+            {
+                Nombre = nombreEntry.Text,
+                Apellido = apellidoEntry.Text,
+                Telefono = telefonoEntry.Text,
+                Email = emailEntry.Text
+            };
+
+            using (var conn = new SQLite.SQLiteConnection(App.Ruta_DB))
+            {
+                conn.CreateTable<Contacto>();
+                conn.Insert(nuevoContacto);
+            }
+           
+        }
+
+    }
 }
